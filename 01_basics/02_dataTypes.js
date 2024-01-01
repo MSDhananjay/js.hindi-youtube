1e3ba71ced3c4aef2ff4257ca02440c2a1e786aa
MSDhananjay <dhananjay97@gmail.com>
2024-01-01 03:46:20 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3); // we are using nodejs ; not browser;
+
+console.log(3
+            + 
+             3)  // code readability should have high priority.
+
+console.log("Dhananjay")
+
+let name = "Dhananjay"
+
+let age = 18
+
+let isLoggedIn = false
+let state;
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null  => object // standalone value
+// undefined => value has not be assigned here state;
+// symbol => unique
+
+// object
+
+console.log(typeof "Dhananjay")
+console.log(typeof age);
+
+console.log(typeof null) // type is object
+console.log(typeof undefined) // type is undefined
